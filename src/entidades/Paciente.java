0fa8d81ce26b19804e18bec78340f9de5b152bdc
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2025-06-07 10:42:52 -0300

    criando estrutura de pastas

---
+++ src/entidades/Paciente.java
@@ -0,0 +1,37 @@
+package entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paciente {
+    private String nome;
+    private String id;
+    private List<DispositivoDeMonitoramento> dispositivos;
+
+    public Paciente(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+        this.dispositivos = new ArrayList<>();
+    }
+
+    public void adicionarDispositivo(DispositivoDeMonitoramento d) {
+        dispositivos.add(d);
+    }
+
+    public void removerDispositivo(DispositivoDeMonitoramento d) {
+        dispositivos.remove(d);
+    }
+
+    public List<Dado> gerarLeituras() {
+        List<Dado> leituras = new ArrayList<>();
+        for (DispositivoDeMonitoramento dispositivo : dispositivos) {
+            leituras.add(dispositivo.gerarDadoAleatorio());
+        }
+        return leituras;
+    }
+
+    // Getters e Setters
+    public String getNome() { return nome; }
+    public String getId() { return id; }
+    public List<DispositivoDeMonitoramento> getDispositivos() { return dispositivos; }
+}
